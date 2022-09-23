f044ec9919f017b05ba40f351f3fcfb838f48920
Agustin Savino <110129241+Airiled@users.noreply.github.com>
2022-09-22 23:53:52 -0300

    C10: Arreglando error de carga duplicada de datos

--- src/main.js
+++ src/main.js
@@ -12,12 +12,10 @@ async function getTrendingMovies(){ //Nos trae las peliculas en tendencia
     const { data } = await api(`trending/movie/day`); // {  data    } con este obtenemos el objeto data de la url a la que estamos
                                                       //haciendo la solicitud (tambien hay otros valores aparte de data que podemos obtener)
 
+    trendingMoviesPreviewList.innerHTML ="";
+    
 
     const movies = data.results;
-    console.log({movies});
-
-    const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList')
-
     movies.forEach(movie => { //Por cada pelicula le creamos una estructura HTML
         
         const divMovie = document.createElement('div');
@@ -36,13 +34,10 @@ async function getTrendingMovies(){ //Nos trae las peliculas en tendencia
 async function getCategoriesPreviewThemes(){ //Nos trae las categorias y sus temas(colores)
     const { data } = await api(`genre/movie/list`); 
    
+    categoriesPreviewSection.innerHTML = "";
+
     const categories = data.genres;
-    console.log({categories});
-
-    const categoriesPreviewSection = document.querySelector('#categoriesPreview .categoriesPreview-list');
-    console.log(categoriesPreviewSection);
-
-    categories.forEach(category => { //Por cada pelicula le creamos una estructura HTML
+    data.genres.forEach(category => { //Por cada pelicula le creamos una estructura HTML
         
         const divCategory = document.createElement('div');
         divCategory.classList.add('category-container');
